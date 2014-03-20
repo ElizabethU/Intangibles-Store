80b8d237645c007ea361b8dc91e26331d47efcdf
Elizabeth Uselton <elizabeth.uselton@gmail.com>
2014-03-20 09:59:14 -0700

    increments quantity in cart

--- app/scripts/routes/product.js
+++ app/scripts/routes/product.js
@@ -6,16 +6,26 @@ App.ProductRoute = Ember.Route.extend({
   actions: {
     addToCart: function(product){
       var cart = this.modelFor('application')
-      var item = this.store.createRecord('item', { 
-          quantity: 1,
-          current_price: product.get('price'),
-          product: product,
-          cart: cart
-        }); 
-        cart.get('items').then(function(items) { 
-          items.pushObject(item);
+      var store = this.store
+      cart.get('items').then(function (items) {
+        return items.find(function (item) {
+          return item.get('product').get('id') == product.get('id')
         })
-        item.save();
+      }).then(function (item) {
+        if (item) {
+          item.incrementProperty('quantity');
+          item.save();
+        } else {
+          var item = store.createRecord('item', {
+            cart: cart, 
+            product: product, 
+            quantity: 1, 
+            current_price: product.get('price')
+          });
+          cart.get('items').pushObject(item)
+          item.save();
+        }
+      })
 
     this.transitionTo('cart')
     }
